fa75686bc72856df71b01733ca444bc2b00d0974
Sebquil <33837727+Sebquil@users.noreply.github.com>
2018-05-25 15:07:06 +0200

    test

--- riskIsep/src/Main.java
+++ riskIsep/src/Main.java
@@ -4,9 +4,6 @@ public class Main {
 	
 		 
 	public static void main(String[] args) {
-		
-		creationJeu();
-		
 		int nbJoueurs=5;
 		ArrayList<Player> tableauJoueurs= new ArrayList<Player>();
 		// demander de rentrer le nombre de joueurs qui jouent
@@ -109,20 +106,5 @@ public class Main {
 			}
 	
 		}
-	}
-	
-	public static void creationJeu() {
-		int[] listSAmericaTerritory = {1,2,3,4};
-		int[] listNAmericaTerritory = {5,6,7,8,9,10,11,12,13};
-		int[] listEuropeTerritory = {14,15,16,17,18,19,20};
-		int[] listAsiaTerritory = {21,22,23,24,25,26,27,28,29,30,31,32};
-		int[] listOceaniaTerritory = {33,34,35,36};
-		int[] listAfricaTerritory = {37,38,39,40,41,42};
-		Region NAmerica = new Region(1,4,listSAmericaTerritory);
-		Region SAmerica = new Region(2,9,listNAmericaTerritory);
-		Region Europe = new Region(3,7,listEuropeTerritory);
-		Region Asia = new Region(4,12,listAsiaTerritory);
-		Region Oceania = new Region(5,4,listOceaniaTerritory);
-		Region Africa = new Region(6,6,listAfricaTerritory);
 	}
 }
