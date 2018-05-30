ceefe9a55bb797fbc85753a840f711583d191a78
Sebquil <33837727+Sebquil@users.noreply.github.com>
2018-05-30 11:12:39 +0200

    main

--- riskIsep/src/Main1.java
+++ riskIsep/src/Main1.java
@@ -1,15 +1,7 @@
-import java.util.ArrayList;
-
 import javax.swing.JOptionPane;
 
-//import edu.princeton.cs.introcs.StdDraw;
-
-
-
-
-public class Main {
+public class Main1 {
 	static Interface interfMap;
-	static Interface interfNbJoueurs;
 	static Territory terOrigin;
 	static Territory terCibl;
 	int[] listSAmericaTerritory = {1,2,3,4};
@@ -109,158 +101,46 @@ public class Main {
 	static Territory Egypt = new Territory(41, Adj41);
 	static Territory Maghreb = new Territory(42, Adj42);
 	static Territory[] territoryList = {Bresil,Argentine,Venezuela,Perou,Mexique,WUS,EUS,Quebec,Ontario,Alberta,NCanada,Alaska,Groenland,Islande,Scandinavie,UK,Russie,Allemagne,France,Italie,Arabie,Asia22,Asia23,Asia24,Asia25,Asia26,Asia27,Asia28,Chine,Inde,Asia31,Japon,Oceania33,NewZeland,EAustralia,WAustralia,Madagascar,SAfrica,Congo,Somalie,Egypt,Maghreb};
-
-		 
 	
-		
+	
+	public static void main(String[] args) {
 		String[] choices = {"2", "3", "4", "5", "6"};
 		int res = Integer.parseInt((String)JOptionPane.showInputDialog(JOptionPane.getRootFrame(), "Combien de joueurs ?", "Initialisation", JOptionPane.PLAIN_MESSAGE, null, choices, "2"));
 		
-		// choix du nombre de joueurs qui jouent
-		interfNbJoueurs=new Interface(500,500);
-		interfNbJoueurs.ecranNbJoueurs();
-		int nbJoueurs=0;
-		nbJoueurs=Interface.posThib();
-		System.out.println(nbJoueurs);
-		
-
-		// tableau des jouerus
-		ArrayList<Player> tableauJoueurs= new ArrayList<Player>();
-
-		
-		
 		interfMap = new Interface(1080,720);
 		interfMap.ecranCarte();
-		
-		Interface.positionSouris();
-		
-		
-		// demander de rentrer le nombre de joueurs qui jouent
+		int choix;
+		choix = Interface.actionChoice();
 
-		
-		// creation des différents joueurs 
-		for (int i=0;i<nbJoueurs;i++){
-			Missions m = Missions.giveAMission(nbJoueurs, Missions.missionTab);
-			
-			Player p= new Player ();
-			
-			tableauJoueurs.add(p);
-			
-		}
-		
-		// creation de l'interface graphique
-		interfMap = new Interface(1080,720);
-		interfMap.ecranCarte();
-		
-		Interface.positionSouris();
-		// phase d'initialisation : On pose aléatoirement des soldats sur les terrritoires tour à tour
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// phase réelle de jeu qui ne s'arrete pas tant que aucun joueur n'a pas gagné
-		
-		
-		boolean victoire =false;
-		int choix ;
-		while (!victoire){
-			for (int j=0;j<nbJoueurs;j++){
-				// Etape de renforts
+		// s'il veut déplacer des troupes 
+			if (choix==0)
 				
-				
-				choix=-1;
-				
-				while (choix!=2)
-				{
-				// le joueur décide s'il veut attaquer ou déplacer des troupes 
-				//0 si déplacement 
-				//1 si attaque 
-				//2 si passer son tour
-				
-				choix = Interface.actionChoice();
-
-				// s'il veut déplacer des troupes 
-					if (choix==0)
-						
-					{
-						int terOrigine= Interface.territoryChoice();
-						for (int i=0; i<territoryList.length;i++) {
-							if (terOrigine==i) {
-								terOrigin = territoryList[i];
-							}
-						}
-						if (Player.isControlledTerritory(terOrigin.getTerritoryNumber())) {
-							int terCible = Interface.territoryChoice();
-							for (int i=0; i<territoryList.length;i++) {
-								if (terCible==i) {
-									terCibl = territoryList[i];
-								}
-							}
-							int nGuns = 0;
-							int nCavalry = 0;
-							int nSoldiers = 0;
-							if (Territory.moveArmy(terOrigin,terCibl,nGuns,nCavalry,nSoldiers))
-							{
-								System.out.println("Le déplacement a été effectué");
-							}
-						}
-					// il choisit le territoire duquel les soldats doivent partir
-				
-					// il choisit combien d'unités il veut déplacer
-				
-				
-						
-
-				
+			{
+				int terOrigine= Interface.territoryChoice();
+				for (int i=0; i<territoryList.length;i++) {
+					if (terOrigine==i) {
+						terOrigin = territoryList[i];
 					}
-				
-					/*else if (choix==1)
-					{
-					// nous sommes dans le cas d'une attaque 
-					// choix du territoire depuis lequel on attaque
-					// choix du territoire a attaquer 
-					// choix des unités attaquantes
-						terOrigin= Interface.territoryChoice();
-						if (Player.isControlledTerritory(terOrigine.getTerritoryNumber())) {
-							Territory terCible = Interface.territoryChoice();
-							int nGunsAttaquant = terOrigine.getnGuns();
-							int nCavalryAttaquant = terOrigine.getnCavalry();
-							int nSoldiersAttaquant = terOrigine.getnSoldiers();
-					
-						if (Territory.attackTerritoryCheck(terOrigine,terCible,nGunsAttaquant, nCavalryAttaquant,nSoldiersAttaquant ))
-						{
-						
-						// les unités jouent
-						
-						
-							if (Territory.territoryWins(terOrigine,  terCible, nGunsRestants,nCavalryRestants, nSoldiersRestants))
-							{
-								System.out.println("Le territoire est gagné");
-							}
-						}
-						}
-					
-					}
-					
-					else if (choix==2)
-					{
-						// on est dans le cas ou le joueur a fini son tour 
-						// on vérifie qu'il n'a pas gagné
-						if (isMissionSuceeded(tableauJoueurs.get(j)))
-						{
-							victoire=true;
-						}
-					}*/
 				}
+				if (Player.isControlledTerritory(terOrigin.getTerritoryNumber())) {
+					int terCible = Interface.territoryChoice();
+					for (int i=0; i<territoryList.length;i++) {
+						if (terCible==i) {
+							terCibl = territoryList[i];
+						}
+					}
+					int nGuns = 0;
+					int nCavalry = 0;
+					int nSoldiers = 0;
+					if (Territory.moveArmy(terOrigin,terCibl,nGuns,nCavalry,nSoldiers))
+					{
+						System.out.println("Le déplacement a été effectué");
+					}
+				}
+			// il choisit le territoire duquel les soldats doivent partir
 		
+			// il choisit combien d'unités il veut déplacer
 			}
-	
-		}
 	}
-		
+	
 }
